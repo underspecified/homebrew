ac5fe74b6eb524a230b24d29c13b9d0917f2e8fe
underspecified <underspecified@gmail.com>
2011-09-08 23:06:02 +0900

    Added formula for GENIA Tagger, the English part-of-speech tagger

---
+++ Library/Formula/geniatagger.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Geniatagger < Formula
+  url 'http://www-tsujii.is.s.u-tokyo.ac.jp/~genia/postagger/geniatagger-3.0.1.tar.gz'
+  homepage 'http://www-tsujii.is.s.u-tokyo.ac.jp/GENIA/tagger/'
+  md5 'd7cc73d996a15f73ff8535ae173d2222'
+
+  def install
+    # hardwire the path to the models
+    ['main.cpp', 'morph.cpp', 'namedentity.cpp'].each { 
+      |f| inreplace f, '"./', "\"#{lib}"
+    }        
+
+    system "make geniatagger"
+    lib.install Dir['models_*', 'morphdic']
+    bin.install Dir['geniatagger']
+  end
+end
