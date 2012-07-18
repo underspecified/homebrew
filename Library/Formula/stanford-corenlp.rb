a94252b5f269ad147766a58e5e391a6d5e301558
underspecified <underspecified@gmail.com>
2012-07-18 18:29:21 +0900

    stanford-corenlp: updated to v1.3.3

--- Library/Formula/stanford-corenlp.rb
+++ Library/Formula/stanford-corenlp.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class StanfordCorenlp < Formula
-  url 'http://nlp.stanford.edu/software/stanford-corenlp-2012-01-08.tgz'
+  url 'http://nlp.stanford.edu/software/stanford-corenlp-2012-07-09.tgz'
   homepage 'http://nlp.stanford.edu/software/corenlp.shtml'
-  md5 '0c5a5db07cb66b20815402ed5472dbf0'
-  version '1.3.0'
+  md5 'bc34347cb5f0d58aa7c293a2f5190ff2'
+  version '1.3.3'
 
   def install
     prefix.install Dir['LIBRARY-LICENSES', 'LICENSE.txt', 'README.txt', ]
     bin.install Dir['corenlp.sh']
     libexec.install Dir['CoreNLP-to-HTML.xsl', 'joda-time.jar', 'xom.jar',
-			'stanford-corenlp-2012-01-08.jar', 'stanford-corenlp-2012-01-08-javadoc.jar', 
-			'stanford-corenlp-2011-12-27-models.jar']
+			'stanford-corenlp-2012-07-09.jar', 'stanford-corenlp-2012-07-09-javadoc.jar', 
+			'stanford-corenlp-2012-07-06-models.jar']
   end
 end
