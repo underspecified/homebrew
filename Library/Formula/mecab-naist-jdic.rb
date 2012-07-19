2865972f03b603db1e50d3fc1bd514fcd7fa8e8f
underspecified <underspecified@gmail.com>
2012-07-19 18:36:24 +0900

    mecab-naist-jdic: updated to v0.6.3b-20111013

--- Library/Formula/mecab-naist-jdic.rb
+++ Library/Formula/mecab-naist-jdic.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class MecabNaistJdic < Formula
-  url 'http://sourceforge.jp/frs/redir.php?f=%2Fnaist-jdic%2F48487%2Fmecab-naist-jdic-0.6.3-20100801.tar.gz'
+  url 'http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fnaist-jdic%2F53500%2Fmecab-naist-jdic-0.6.3b-20111013.tar.gz'
   homepage 'http://sourceforge.jp/projects/naist-jdic/'
-  md5 '30af2251331b9d6141748caf033821e3'
+  md5 '52238fb14d949e49a65a9dbc1f7e382c'
 
   depends_on 'mecab'
 
@@ -11,8 +11,8 @@ class MecabNaistJdic < Formula
     args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
     args << ARGV.find(Proc.new {"--with-charset=utf8"}) { |arg| /^--with-charset/.match(arg) }
     system "./configure", *args
-    inreplace 'Makefile.am', '$(DESTDIR)', '/usr/local/Cellar/mecab/0.98'
-    system "/usr/bin/autoreconf"
+    inreplace 'Makefile.am', '$(DESTDIR)', '/usr/local/Cellar/mecab/0.994'
+    system "autoreconf"
     system "make install"
   end
 end
