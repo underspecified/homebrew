ce726edfa1782d635f45edede2d57d31ce2ac1ae
underspecified <underspecified@gmail.com>
2012-07-23 16:06:31 +0900

    stanford-corenlp: fixed helper script

--- Library/Formula/stanford-corenlp.rb
+++ Library/Formula/stanford-corenlp.rb
@@ -6,6 +6,11 @@ class StanfordCorenlp < Formula
   md5 'bc34347cb5f0d58aa7c293a2f5190ff2'
   version '1.3.3'
 
+  def patches
+    # fix paths in utility script
+    DATA
+  end
+
   def install
     prefix.install Dir['LIBRARY-LICENSES', 'LICENSE.txt', 'README.txt', ]
     bin.install Dir['corenlp.sh']
@@ -14,3 +19,26 @@ class StanfordCorenlp < Formula
 			'stanford-corenlp-2012-07-06-models.jar']
   end
 end
+
+__END__
+diff --git i/corenlp.sh w/corenlp.sh
+index 34edb5c..b7eeb75 100755
+--- i/corenlp.sh
++++ w/corenlp.sh
+@@ -1,11 +1,12 @@
+-#!/usr/bin/env bash
++#!/bin/bash
+ #
+ # Runs Stanford CoreNLP.
+ # Simple uses for xml and plain text output to files are:
+ #    ./corenlp.sh -file filename
+ #    ./corenlp.sh -file filename -outputFormat text 
+ 
+-scriptdir=`dirname $0`
++scriptdir=$(grealpath "$(dirname $(grealpath $0))/../libexec")
+ 
+-echo -mx3g -cp "$scriptdir/*.jar" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
+-java -mx3g -cp "$scriptdir/*.jar" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
++cmd="java -mx3g -cp \"$scriptdir/*\" edu.stanford.nlp.pipeline.StanfordCoreNLP $*"
++echo $cmd
++eval $cmd
